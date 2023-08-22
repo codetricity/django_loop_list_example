9fe808331f6bdd6adb00fb75c0c9e89103335a1d
Craig <craig@oppkey.com>
2023-08-22 15:09:35 -0700

    added date to listing

--- did/article/models.py
+++ did/article/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.db import models
+from django.urls import reverse
 
 class Article(models.Model):
-    title = models.CharField(max_length=120, default='title')
+    title = models.CharField(max_length=120, unique=True)
     body = models.TextField(default='body')
+    slug = models.SlugField(max_length=250, blank=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
     
+
+    class Meta:
+        ordering = ['-updated']
+
+    def get_absolute_url(self):
+        return reverse("article:model_detail", kwargs={"id": self.id})
+        
